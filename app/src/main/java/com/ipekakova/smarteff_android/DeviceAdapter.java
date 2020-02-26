c08d6a9e02e3feea9a99f6dbf3b383f953e0c801
ipekakova <iakova97@gmail.com>
2020-02-27 00:52:57 +0300

    Added time picker dialog for suspension.

--- app/src/main/java/com/ipekakova/smarteff_android/DeviceAdapter.java
+++ app/src/main/java/com/ipekakova/smarteff_android/DeviceAdapter.java
@@ -1,9 +1,12 @@
 package com.ipekakova.smarteff_android;
 
 import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,9 +15,11 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 /**
  * Created by User on 2.02.2020.
@@ -53,57 +58,63 @@ public class DeviceAdapter extends ArrayAdapter<Device> {
             holder.button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    final Dialog dialog = new Dialog(context);
-                    dialog.setContentView(R.layout.suspend_dialog);
 
-                    // custom dialog elemanlarını tanımla - text, image ve button
-                    Button btnKaydet = (Button) dialog.findViewById(R.id.button_kaydet);
-                    Button btnIptal = (Button) dialog.findViewById(R.id.button_iptal);
-                    TextView tvBaslik = (TextView) dialog.findViewById(R.id.textview_baslik);
-                    ImageView ivResim = (ImageView) dialog.findViewById(R.id.imageview_resim);
-                    // custom dialog elemanlarına değer ataması yap - text, image
-                    tvBaslik.setText("Are you sure?");
-                    // tamam butonunun tıklanma olayları
-                    btnKaydet.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            Device device = devices.get(position);
-                            if (device.getAutomation() == R.drawable.radio_red){
-                                Log.i("red", "Clicked on ENABLE button");
-                                device.setAutomation(R.drawable.radio_green);
-                                device.setSuspendOrEnable("Suspend");
+                    final Device device = devices.get(position);
+                    // If selected device button is "Enable" then don't show any dialog, directly activate the automated shutdown
+                    if (device.getAutomation() == R.drawable.radio_red){
+                        Log.i("red", "Clicked on ENABLE button");
+                        device.setAutomation(R.drawable.radio_green);
+                        device.setSuspendOrEnable("Suspend");
+                        holder.automation.setImageResource(device.getAutomation());
+                        holder.button.setText(device.getSuspendOrEnable());
+                        notifyDataSetChanged();
+                        Toast.makeText(context, "Enabled shutdown", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        final Dialog dialog = new Dialog(context);
+                        dialog.setContentView(R.layout.suspend_dialog);
 
-                                Toast.makeText(context, "Enabled shutdown", Toast.LENGTH_SHORT).show();
-                            }
-                            else {
+                        // custom dialog elemanlarını tanımla - text, image ve button
+                        Button btnKaydet = (Button) dialog.findViewById(R.id.button_kaydet);
+                        Button btnIptal = (Button) dialog.findViewById(R.id.button_iptal);
+                        TextView tvBaslik = (TextView) dialog.findViewById(R.id.textview_baslik);
+                        final TimePicker timePicker = (TimePicker) dialog.findViewById(R.id.imageview_resim);
+                        // custom dialog elemanlarına değer ataması yap - text, image
+                        tvBaslik.setText("Are you sure?");
+                        // tamam butonunun tıklanma olayları
+                        btnKaydet.setOnClickListener(new View.OnClickListener() {
+                            @RequiresApi(api = Build.VERSION_CODES.M)
+                            @Override
+                            public void onClick(View v) {
                                 Log.i("red", "Clicked on SUSPEND button");
                                 // Automation is on ise kapatti.
                                 device.setSuspendOrEnable("Enable");
                                 device.setAutomation(R.drawable.radio_red);
                                 Toast.makeText(context, "Suspended automated shutdown", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(context, timePicker.getHour() +": "+ timePicker.getMinute(), Toast.LENGTH_SHORT).show();
+                                holder.automation.setImageResource(device.getAutomation());
+                                holder.button.setText(device.getSuspendOrEnable());
+                                notifyDataSetChanged();
+                                dialog.dismiss();
                             }
+                        });
 
-                            holder.automation.setImageResource(device.getAutomation());
-                            holder.button.setText(device.getSuspendOrEnable());
-                            notifyDataSetChanged();
+                        // iptal butonunun tıklanma olayları
+                        btnIptal.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                dialog.dismiss();
+                            }
+                        });
+                        dialog.show();
+                        Log.i("green", "Clicked on SUSPEND button");
+                        // Automation is on ise kapatti.
+                        device.setSuspendOrEnable("Enable");
+                        device.setAutomation(R.drawable.radio_red);
+                    }
 
-                        }
-                    });
-                    // iptal butonunun tıklanma olayları
-                    btnIptal.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            dialog.dismiss();
-                        }
-                    });
-
-                    dialog.show();
-
-                    Toast.makeText(context, "Clicked on BUTTON", Toast.LENGTH_LONG).show();
-                    //Toast.LENGTH_LONG yerine 2000 girersek 2 sn gösterecektir.
                 }
             });
-            //holder.enableShutDown = (Switch) convertView.findViewById(R.id.enable_switch);
             convertView.setTag(holder);
         }
         else{
@@ -137,7 +148,28 @@ public class DeviceAdapter extends ArrayAdapter<Device> {
     @Override
     public long getItemId(int position) {
         return devices.get(position).hashCode();
-    }
+    }}
 
 
-}
+
+ /*         Alternatif TimePicker Dialog
+                    final Calendar cldr = Calendar.getInstance();
+                    int hour = cldr.get(Calendar.HOUR_OF_DAY);
+                    int minutes = cldr.get(Calendar.MINUTE);
+                    TimePickerDialog tpd = new TimePickerDialog(context,
+                            new TimePickerDialog.OnTimeSetListener() {
+                                @Override
+                                public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                    // hourOfDay ve minute değerleri seçilen saat değerleridir.
+                                    // Edittextte bu değerleri gösteriyoruz.
+                                    Toast.makeText(context, hourOfDay +": "+ minute, Toast.LENGTH_SHORT).show();
+
+                                }
+                            },hour,minutes, true);
+                    // dialog penceresinin button bilgilerini ayarlıyoruz ve ekranda gösteriyoruz.
+                    tpd.setButton(TimePickerDialog.BUTTON_POSITIVE, "Kaydet", tpd);
+                    tpd.setButton(TimePickerDialog.BUTTON_NEGATIVE, "İptal", tpd);
+                    tpd.onClick(tpd, TimePickerDialog.BUTTON_POSITIVE){
+                    }
+                    tpd.show();
+                    */
