c0ab849cbc11a7016cde7c7f4278da606d921620
ipekakova <iakova97@gmail.com>
2020-03-12 00:39:42 +0300

    When a notification is received from service, a dialog is displayed.

--- app/src/main/java/com/ipekakova/smarteff_android/MyFirebaseMessagingService.java
+++ app/src/main/java/com/ipekakova/smarteff_android/MyFirebaseMessagingService.java
@@ -1,5 +1,6 @@
 package com.ipekakova.smarteff_android;
 
+import android.app.AlertDialog;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
@@ -10,10 +11,14 @@ import android.net.Uri;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Map;
 
 /**
  * Created by User on 8.03.2020.
@@ -28,19 +33,25 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
      * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
      */
     // [START receive_message]
-
+// Foreground'dayken çalışır. Yani uygulama ekranındaysa o an çalışıyor.
     public void onMessageReceived(RemoteMessage remoteMessage) {
-
         // TODO(developer): Handle FCM messages here.
         // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         Log.d(TAG, "From: " + remoteMessage.getFrom());
-
         // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
-            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData().get("device_id"));
 
-            sendNotification(remoteMessage.getNotification().getBody());
-
+            Intent intent = new Intent(MyFirebaseMessagingService.this,ShowNotificationActivity.class);
+            Map<String, String> data = remoteMessage.getData();
+            String user_name = data.get("user_name");
+            String device_id = data.get("device_id");
+            // add data
+            intent.putExtra("user_name", user_name);
+            intent.putExtra("device_id", device_id);
+            //LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+            startActivity(intent);
+            //sendNotification(remoteMessage.getNotification().getBody());
         }
 
         // Check if message contains a notification payload.
@@ -56,23 +67,26 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
      * @param messageBody FCM message body received.
      */
     private void sendNotification(String messageBody) {
-        Intent intent = new Intent(this, MainActivity.class);
+        Log.i( "sendNotification: " , messageBody);
+
+        Intent intent = new Intent(this, ShowDevicesActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
                 PendingIntent.FLAG_ONE_SHOT);
 
         Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, "channel_id")
                 .setContentTitle("FCM Message")
                 .setContentText(messageBody)
+                .setSmallIcon(R.drawable.suspend)
                 .setAutoCancel(true)
                 .setSound(defaultSoundUri)
                 .setContentIntent(pendingIntent);
 
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
         notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+
     }
 
 }
