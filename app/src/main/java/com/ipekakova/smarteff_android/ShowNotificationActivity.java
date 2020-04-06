bb591706c12910010507e40c3b9a0219bafe928a
ipekakova <iakova97@gmail.com>
2020-04-06 18:26:34 +0300

    Added form to specify the suspension time in notificationDialog

--- app/src/main/java/com/ipekakova/smarteff_android/ShowNotificationActivity.java
+++ app/src/main/java/com/ipekakova/smarteff_android/ShowNotificationActivity.java
@@ -2,11 +2,14 @@ package com.ipekakova.smarteff_android;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 /**
@@ -17,18 +20,32 @@ public class ShowNotificationActivity extends AppCompatActivity implements View.
 
     Dialog dialog;
     Button yes,no;
+    TextView tv_device_info;
     String TAG = "NotificationActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.notification_dialog);
+        setContentView(R.layout.notification_dialog);
         AlertDialog.Builder builder = new  AlertDialog.Builder(this);
+        tv_device_info = (TextView) findViewById(R.id.tv_device_info);
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        String date = bundle.getString("date");
+        int device_id = Integer.parseInt(bundle.getString("device_id"));
+        Log.i("showNotification: ", date+ device_id);
+        tv_device_info.setText("Your device D:" + device_id + "will be automatically shutdown on: "+ date);
+        /*
         View view = getLayoutInflater().inflate(R.layout.notification_dialog,null);
         builder.setView(view);
         AlertDialog dialog = builder.create();
         dialog.show();
-        yes = (Button) view.findViewById(R.id.start_compaign_yes);
-        no = (Button)view. findViewById(R.id.start_compaign_no);
+        yes = (Button) view.findViewById(R.id.button_suspend);
+        no = (Button)view. findViewById(R.id.button_dismiss_suspend);
+        yes.setOnClickListener(this);
+        no.setOnClickListener(this);
+        */
+        yes = (Button) findViewById(R.id.button_suspend);
+        no = (Button) findViewById(R.id.button_dismiss_suspend);
         yes.setOnClickListener(this);
         no.setOnClickListener(this);
     }
@@ -36,11 +53,32 @@ public class ShowNotificationActivity extends AppCompatActivity implements View.
     @Override
     public void onClick(View v) {
 
-        if (v.getId() == R.id.start_compaign_yes){
-            Log.e(TAG,"customer press yes.");
+        if (v.getId() == R.id.button_suspend){
+            Log.e(TAG,"Suspendend.");
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            // 2. Chain together various setter methods to set the dialog characteristics
+            builder.setTitle("Select a time for suspend your shutdown:");
+                    builder.setSingleChoiceItems(R.array.suspend_hour_options, -1 ,new DialogInterface.OnClickListener() {
+                        String[] hours = getResources().getStringArray(R.array.suspend_hour_options);
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int which) {
 
-        }else if (v.getId() == R.id.start_compaign_no){
-            Log.e(TAG,"customer press no.");
+                            Log.d(TAG, hours[which]+ " suspended.");
+
+                        }
+                    });
+            builder.setPositiveButton("Select", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                    //startActivity(intent);
+
+                }
+            });
+            builder.show();
+
+        }else if (v.getId() == R.id.button_dismiss_suspend){
+            Log.e(TAG,"Discard notification.");
         }
     }
 }
