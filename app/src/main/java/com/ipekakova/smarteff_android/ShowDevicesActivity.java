e545671209c9b2377916e785e40d3bb50ea1e5cf
ipekakova <iakova97@gmail.com>
2020-04-08 17:40:04 +0300

    Added Service for device updates from server.

--- app/src/main/java/com/ipekakova/smarteff_android/ShowDevicesActivity.java
+++ app/src/main/java/com/ipekakova/smarteff_android/ShowDevicesActivity.java
@@ -9,7 +9,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
-import android.support.v4.content.LocalBroadcastManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -18,6 +17,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -43,7 +43,7 @@ public class ShowDevicesActivity extends AppCompatActivity implements Navigation
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_all_devices);
         setContentView(R.layout.activity_nav_main);
-
+        startIntentService();
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
@@ -55,22 +55,32 @@ public class ShowDevicesActivity extends AppCompatActivity implements Navigation
         http = new HttpGetAsyncTask(this);
         //currentUser = (User) intent.getExtras().getSerializable("logged_in_user");
         navUsername.setText("Logged in as: "+ currentUser.getName().toUpperCase());
-
         Log.i("Logged in user infos:", currentUser.toString());
-        initialize();
+        try {
+            initialize();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         Log.i("oncreate-devices",currentUser.getDevices().toString());
         //TODO
 
     }
 
-    private void initialize() {
+    public void startIntentService() {
+        Intent intent = new Intent( ShowDevicesActivity.this, UpdateDevicesService.class);
+        intent.putExtra("sleepTime", 50);
+        startService(intent);
+    }
+
+    private void initialize() throws ExecutionException, InterruptedException {
         listView = (ListView) findViewById(R.id.device_list);
-        currentUser.setDevices(getUserDevices());
-        //Log.i("initialize-devices",currentUser.getDevices().toString());
+        String devicesString  = (String) http.execute( getString(R.string.get_devices_url)+ "/"+ currentUser.getId() ).get();
+        currentUser.setDevices(parseJsonForDevices(devicesString));
         listViewAdapter = new DeviceAdapter(ShowDevicesActivity.this, currentUser.getDevices(),currentUser);
         listView.setAdapter(listViewAdapter);
         Log.i("currentUser", String.valueOf(currentUser.getClass()));
-        //usernameTv.setText("Logged in as: "+currentUser.getName());
 
     }
 
@@ -78,91 +88,83 @@ public class ShowDevicesActivity extends AppCompatActivity implements Navigation
         @Override
         protected void onResume() {
             super.onResume();
-            currentUser.setDevices(listViewAdapter.devices);
-            LocalBroadcastManager.getInstance(this).registerReceiver(broadcastReceiverPost, new IntentFilter("json"));
-
+            //currentUser.setDevices(listViewAdapter.devices);
+            //LocalBroadcastManager.getInstance(this).registerReceiver(broadcastReceiverPost, new IntentFilter("json"));
             //listViewAdapter.notifyDataSetChanged();
             //Log.i("onresume-devices", currentUser.getDevices().toString());
+
+            //Broadcast receiver’ı register ederken bir intent filter oluşturmamız ve bu intent filter’a bir action belirtmemiz gerekir.
+            // Bu sayede broadcast receiverın OnReceive methodu hangi olayda tetiklenecekse bunu android işletim sistemine belirtmiş oluruz.
+            IntentFilter filter=new IntentFilter();
+            filter.addAction("my.action");
+            registerReceiver(myBroadcastReceiver, filter);
         }
 
     @Override
     public void onPause() {
         super.onPause();
-        LocalBroadcastManager.getInstance(this).unregisterReceiver(broadcastReceiverPost);
-
-    }private ArrayList<Device> getUserDevices(){
-    ArrayList<Device> devices = new ArrayList<Device>();
-    try {
-        //String devicesString  = (String) http.execute( getString(R.string.get_devices_url) ).get();
-        String devicesString  = (String) http.execute( getString(R.string.get_devices_url)+ "/"+ currentUser.getId() ).get();
-        Log.i("devices_url", getString(R.string.get_devices_url)+ "/"+ currentUser.getId());
-        try {
-            while (true) {
-                JSONObject object = new JSONObject(devicesString);
-                JSONArray devicesArray = new JSONArray(object.get("devices").toString());
-                Log.i("devicesArray", devicesArray.toString());
-
-                for (int i = 0 ; i < devicesArray.length() ; i++) {
-                    JSONObject obj = new JSONObject(devicesArray.get(i).toString());
-                    Log.i("obj",obj.toString());
-
-                    Integer deviceId = (Integer) obj.get("id");
-                    Log.i("Device Name: " ,deviceId.toString());
-
-                    String deviceName = obj.get("name").toString();
-                    Log.i("Device Name: " ,deviceName);
-                    //Boolean isOn = obj.getBoolean("isOn");
-                    Integer isOn = (Integer) obj.get("isOn");
-                    JSONObject automationObj = new JSONObject(obj.get("automation").toString());
-                    String suspended = automationObj.get("suspend").toString();
-                    Log.i("suspend", suspended);
-
-                    int isOnView, automationView;
-                    String buttonText;
-                    if (isOn == 1){
-                        isOnView = R.drawable.radio_green;
-                    }else{
-                        isOnView = R.drawable.radio_red;
-                    }
-                    if (suspended.equals("True")){ // Otomatik kapatma ertelendiyse
-                        automationView = R.drawable.radio_red;
-                        buttonText = "Enable";
-                        String expiration = automationObj.get("expiration").toString();
-                        Device suspendendDevice = new SuspendendDevice(deviceId, isOnView, automationView, buttonText, expiration);
-                        devices.add(suspendendDevice);
-                        Log.i("expiration: " , expiration);
-                    }
-                    else{ //Automation active
-                        automationView = R.drawable.radio_green;
-                        buttonText = "Suspend";
-                        Device automatedDevice = new AutomatedDevice(deviceId, isOnView, automationView, buttonText);
-                        devices.add(automatedDevice);
-                    }
-                    //Device device = new Device(name, isOnView, automationView, buttonText);
-                    //devices.add(device);
-                    //usersDictionary.put(id, name);
-                    //Log.i("IsOn: " , String.valueOf(isOn));
-                }
-
-                break;
-            }
-        } catch (JSONException e) {
-            System.out.println(e.getMessage());
-        }
-    } catch (InterruptedException e) {
-        e.printStackTrace();
-    } catch (ExecutionException e) {
-        e.printStackTrace();
+        //LocalBroadcastManager.getInstance(this).unregisterReceiver(broadcastReceiverPost);
+        unregisterReceiver(myBroadcastReceiver);
     }
-    Log.i("get-devices", devices.toString());
-    return devices;
-}
+
+    private ArrayList<Device> parseJsonForDevices(String devicesString){
+        ArrayList<Device> devices = new ArrayList<Device>();
+            try {
+                while (true) {
+                    JSONObject object = new JSONObject(devicesString);
+                    JSONArray devicesArray = new JSONArray(object.get("devices").toString());
+                    Log.i("devicesArray", devicesArray.toString());
+
+                    for (int i = 0 ; i < devicesArray.length() ; i++) {
+                        JSONObject obj = new JSONObject(devicesArray.get(i).toString());
+                        Log.i("obj",obj.toString());
+
+                        Integer deviceId = (Integer) obj.get("id");
+                        Log.i("Device Name: " ,deviceId.toString());
+
+                        String deviceName = obj.get("name").toString();
+                        Log.i("Device Name: " ,deviceName);
+                        //Boolean isOn = obj.getBoolean("isOn");
+                        Integer isOn = (Integer) obj.get("isOn");
+                        JSONObject automationObj = new JSONObject(obj.get("automation").toString());
+                        String suspended = automationObj.get("suspend").toString();
+                        Log.i("suspend", suspended);
+
+                        int isOnView, automationView;
+                        String buttonText;
+                        if (isOn == 1){
+                            isOnView = R.drawable.radio_green;
+                        }else{
+                            isOnView = R.drawable.radio_red;
+                        }
+                        if (suspended.equals("True")){ // Otomatik kapatma ertelendiyse
+                            automationView = R.drawable.radio_red;
+                            buttonText = "Enable";
+                            String expiration = automationObj.get("expiration").toString();
+                            Device suspendendDevice = new SuspendendDevice(deviceId, isOnView, automationView, buttonText, expiration);
+                            devices.add(suspendendDevice);
+                            Log.i("expiration: " , expiration);
+                        }
+                        else{ //Automation active
+                            automationView = R.drawable.radio_green;
+                            buttonText = "Suspend";
+                            Device automatedDevice = new AutomatedDevice(deviceId, isOnView, automationView, buttonText);
+                            devices.add(automatedDevice);
+                        }
+                    }
+
+                    break;
+                }
+            } catch (JSONException e) {
+                System.out.println(e.getMessage());
+            }
+        return devices;
+    }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
         if (id == R.id.nav_devices) {
             listViewAdapter.setDevices(currentUser.getDevices());
             listViewAdapter.notifyDataSetChanged();
@@ -181,13 +183,13 @@ public class ShowDevicesActivity extends AppCompatActivity implements Navigation
             Log.i("suspendedDevices", suspendedDevices.toString());
             listViewAdapter.setDevices(suspendedDevices);
             listViewAdapter.notifyDataSetChanged();
-
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
 
     public Context getActivity() {
         return this.getApplication();
@@ -203,6 +205,21 @@ public class ShowDevicesActivity extends AppCompatActivity implements Navigation
                 Log.i("received broadcast: ", result);
                 listViewAdapter.notifyDataSetChanged();
             }
+        }
+    };
+
+    private BroadcastReceiver myBroadcastReceiver =new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            String updatedDevicesString = intent.getStringExtra("veri");
+            Log.d("ipek","Updated devices :" + updatedDevicesString);
+            ArrayList<Device> updatedDevices =   parseJsonForDevices(updatedDevicesString);
+            currentUser.setDevices(updatedDevices);
+            listViewAdapter.setDevices(currentUser.getDevices());
+            listViewAdapter.notifyDataSetChanged();
+            //Toast.makeText(ShowDevicesActivity.this, "Updated devices :" + updatedDevicesString, Toast.LENGTH_SHORT).show();
+
         }
     };
 }
---
+++ app/src/main/java/com/ipekakova/smarteff_android/UpdateDevicesService.java
@@ -0,0 +1,125 @@
+package com.ipekakova.smarteff_android;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+
+public class UpdateDevicesService extends IntentService {
+
+    private static final String TAG = UpdateDevicesService.class.getSimpleName();
+    SharedPreferences sp;
+    User currentUser;
+
+    public UpdateDevicesService() {
+        super("MyWorkerThread");
+    }
+
+    @Override
+    public void onCreate() {
+        Log.d(TAG, "oncreate methodu " + Thread.currentThread().getName() + " threadi üzerinden cagrildi");
+        super.onCreate();
+        sp = getSharedPreferences("login",MODE_PRIVATE);
+        currentUser = new User(sp.getInt("user_id",0),sp.getString("user_name", "") );
+    }
+
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+
+        Log.i(TAG, "onHandleIntent methodu " + Thread.currentThread().getName() + " threadi üzerinden cagrildi");
+        int sleepTime = intent.getIntExtra("sleepTime", 1);
+        int kontrol = 1;
+
+        while (true) {
+            try {
+                // 1 dakikada bir istek atar.
+                Thread.sleep(60*1000);
+                String devices = getUserDevices();
+                Intent intent1 = new Intent("my.action");
+                intent1.putExtra("veri", devices);
+                sendBroadcast(intent1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            kontrol++;
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(TAG, "onDestroy methodu " + Thread.currentThread().getName() + " threadi üzerinden cagrildi");
+        super.onDestroy();
+    }
+
+    private String getUserDevices(){
+            HttpGet http = new HttpGet();
+        String devicesString  = null;
+        try {
+            devicesString = (String) http.execute( getString(R.string.get_devices_url)+ "/"+ currentUser.getId()).get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        return devicesString;
+    }
+
+    private class HttpGet extends AsyncTask{
+
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            String url = (String ) objects[0];
+            String result = "";
+            String inputLine;
+            try {
+                //Create a URL object holding our url
+                URL myUrl = new URL(url);
+                //Create a connection
+                HttpURLConnection connection =(HttpURLConnection)
+                        myUrl.openConnection();
+                //Connect to our url
+                connection.connect();
+                //Create a new InputStreamReader
+                InputStreamReader streamReader = new InputStreamReader(connection.getInputStream());
+                //Create a new buffered reader and String Builder
+                BufferedReader reader = new BufferedReader(streamReader);
+                StringBuilder stringBuilder = new StringBuilder();
+                //Check if the line we are reading is not null
+                while((inputLine = reader.readLine()) != null){
+                    stringBuilder.append(inputLine).append("\n");            }
+                //Close our InputStream and Buffered reader
+                reader.close();
+                streamReader.close();
+                connection.disconnect();
+                //Set our result equal to our stringBuilder
+                result = stringBuilder.toString();
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+    }
+}
